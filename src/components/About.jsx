47349267d42fb3a900f40ef57b6138554be3e5bc
StevenMadali <stevenmadali17@gmail.com>
2024-12-06 14:27:28 +0800

    Edit About info and Remodel Footer Component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -27,7 +27,7 @@ const About = () => {
   <div id="about"  data-aos="fade-right"  className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
     <div className="max-w-3xl">
       <h2 className="text-3xl text-rose-600 font-bold sm:text-4xl">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quod alias doloribus impedit.
+      "Satisfy your eyes and delight your tummy with IB Bake’s—Oh, so yummy!"
       </h2>
     </div>
 
@@ -43,17 +43,11 @@ const About = () => {
       <div className="lg:py-16">
         <article className="space-y-4 text-gray-600">
           <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut qui hic atque tenetur quis
-            eius quos ea neque sunt, accusantium soluta minus veniam tempora deserunt? Molestiae
-            eius quidem quam repellat.
+          Ib Bake's, owned by a college Filipina homebaker who found herself into baking way back in 2022. Ib Bake's officially starts its journey into business in 2023 and shares her progress on social media that support her growing business. 
           </p>
 
           <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum explicabo quidem
-            voluptatum voluptas illo accusantium ipsam quis, vel mollitia? Vel provident culpa
-            dignissimos possimus, perferendis consectetur odit accusantium dolorem amet voluptates
-            aliquid, ducimus tempore incidunt quas. Veritatis molestias tempora distinctio
-            voluptates sint! Itaque quasi corrupti, sequi quo odit illum impedit!
+          Our goal is to deliver high-quality, freshly baked products crafted with love, meet every customer expectation, and ensure their satisfaction to build their loyalty with us by providing them with extraordinary service and relish. 
           </p>
         </article>
       </div>
