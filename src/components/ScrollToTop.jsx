1b56d019da4e9abc6d68beca78b79ecdfdb8e2bb
StevenMadali <stevenmadali17@gmail.com>
2025-01-01 22:13:32 +0800

    Add Scroll to Top Button Functionality

---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { FaArrowUp } from 'react-icons/fa';
+
+const ScrollToTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <>
+      {isVisible && (
+        <button
+          onClick={scrollToTop}
+          className="fixed bottom-5 right-5 bg-rose-500 text-white p-3 rounded-full shadow-lg hover:bg-rose-600 transition-all duration-300"
+        >
+          <FaArrowUp />
+        </button>
+      )}
+    </>
+  );
+};
+
+export default ScrollToTop;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,17 +7,28 @@ import Carousel from '../components/Carousel'
 import HightlightProduct from '../components/HighlightProduct'
 import About from '../components/About'
 import Contacts from '../components/Contacts'
-
+import ScrollToTop from '../components/ScrollToTop'
 
 const Home = () => {
   return (
-    <>
-     <Navbar />
-    <Carousel />
-    <HightlightProduct />
-    <About/>
-    <Contacts/>
-    </>
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+          <Carousel />
+          <div className="my-8 sm:my-12">
+            <HightlightProduct />
+          </div>
+          <div className="my-8 sm:my-12">
+            <About />
+          </div>
+          <div className="my-8 sm:my-12">
+            <Contacts />
+          </div>
+        </div>
+      </main>
+      <ScrollToTop />
+    </div>
   )
 }
 
